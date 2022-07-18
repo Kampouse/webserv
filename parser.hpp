6967f656347e62e68cbbc23283767d1f951267e6
Jean-philippe Martel <jemartel@c2r5p6.42quebec.com>
2022-07-18 14:45:47 -0400

    added header file

---
+++ parser.hpp
@@ -0,0 +1,27 @@
+#ifndef PARSER_HPP
+# define PARSER_HPP
+# include <fstream> 
+# include <iostream>
+# include <map>
+# include <set>
+# include <vector>
+class parser
+{
+	private:
+		std::ifstream config_file_fd;
+		char *path;
+		std::vector<std::map <std::string, std::string> > config_maps  ;
+	public:
+		parser(char *str);
+		std::string get_server_path(void);
+		void get_server_fields(void);
+
+		parser (parser const &src);
+		parser();
+		~parser(void);
+		parser	&operator = (const parser &copy);
+		void  check_for_error(void);
+
+};
+
+#endif
