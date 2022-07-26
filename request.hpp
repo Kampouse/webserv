97a35ea2944a4073551ac9a01da95b02791141e1
Jean-philippe Martel <jemartel@c2r5p6.42quebec.com>
2022-07-26 19:27:37 -0400

    working?

--- request.hpp
+++ request.hpp
@@ -12,14 +12,15 @@ class request
 		std::string _location;
 		std::string _path;
 		std::string _version;
-		std::map<std::string, std::string>  _headers;
+		std::map<std::string, std::string>  _req_map;
 		public:
 		request(void);
 		request(std::string &request_string);
 		~request(void);
 		request(const request &copy);
-
 		request	&operator = (const request &copy);
+		void	find_host(std::string string) ;
+		void	dispaly_map(void);
 };
 
 #endif
