ffb59d7e057e454b3a4846ef58e28fd2007552bf
Jean-philippe Martel <jemartel@c2r5p6.42quebec.com>
2022-08-17 11:12:25 -0400

    fixing things

--- DumbedDown/utils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <cstring>
-#include <iostream>
-
-typedef std::vector<std::string> StringVector;
-
-StringVector split(std::string line, std::string delimiter);
--- Server.cpp
+++ Server.cpp
@@ -1,211 +1,148 @@
-#include "Server.hpp"
+#include "server.hpp"	
+#include <sstream>
+#include <filesystem>
 
-Server::Server() {}
-Server::~Server() {}
-
-Server::Server(std::string path)
+server::server(server_info servInfo)
 {
-	parser Parsing(path);
-	servers = Parsing.getServers();
-	// printServers();
-}
 
-void Server::connect_servers(void)
-{
-	for (size_t i = 0; i < servers.size(); i++)
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(servInfo.port);
+	server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	serveInfo = servInfo;
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	fcntl(server_fd, F_SETFL, O_NONBLOCK);
+	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &server_addr, sizeof(server_addr));
+	bzero(&(server_addr.sin_zero), 8);
+	if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
 	{
-		if ((servers[i].server_fd = socket(AF_INET, SOCK_STREAM, 0)) <= 2)
-			throw std::runtime_error("Socket failed");
-
-		// Non-blocking socket, only on MacOS
-		fcntl(servers[i].server_fd, F_SETFL, O_NONBLOCK);
-
-		int optval = 1;
-		if (setsockopt(servers[i].server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) < 0)
-			throw std::runtime_error("Failed to set options");
-
-		bzero(&servers[i].address, sizeof(servers[i].address));
-		servers[i].address.sin_family = AF_INET;
-		servers[i].address.sin_port = htons(servers[i].port);
-		servers[i].address.sin_addr.s_addr = inet_addr(servers[i].host.c_str());
-
-		if (canBind(servers[i].port)) {
-			if (bind(servers[i].server_fd, (struct sockaddr *)&servers[i].address, sizeof(servers[i].address)) < 0)
-				throw std::runtime_error("Bind failed");
-		}
-
-		if (listen(servers[i].server_fd, 10) < 0)
-			throw std::runtime_error("Listen failed");
-
-		pollfd newfd;
-		newfd.fd = servers[i].server_fd;
-		newfd.events = POLLIN;
-		newfd.revents = 0;
-		fds.push_back(newfd);
+		return;
 	}
+
+	listen(server_fd, 100);
+	pollfd serv;
+	serv.fd = server_fd;
+	serv.events = POLLIN | POLLHUP | POLLERR;
+	serv.revents = 0;
+	poll_set.push_back(serv);
+	contents.push_back(".css");
+	contents.push_back(".html");
+	contents.push_back(".js");
+	contents.push_back(".png");
+	contents.push_back(".jpg");
+	contents.push_back(".jpeg");
+	contents.push_back(".gif");
+	contents.push_back(".ico");
+
 }
 
-bool Server::canBind(int port)
+void   server::clear_fd (int i)
 {
-	if (!binded_ports.empty()) {
-		std::vector<int>::iterator it = binded_ports.begin();
-		for (; it != binded_ports.end(); it++) {
-			if (*it == port)
-				return (false);
-		}
-	}
-	return (true);
+	close(poll_set[i].fd);
+	poll_set[i].fd = -1;
+	poll_set.erase(poll_set.begin() + i);
 }
 
-void Server::handle_listen(int i)
+void server::add_client (void)
 {
-	sockaddr_storage client_addr;
-	socklen_t addrlen = sizeof(client_addr);
-
-	int client_fd = accept(fds[i].fd, (struct sockaddr*)&client_addr, &addrlen);
-
-	if (client_fd == -1)
-		throw std::runtime_error("Accept Error");
-
-	fds.push_back(pollfd());
-	fds.back().fd = fds[i].fd;
-	fds.back().events = POLLIN;
-	fds.back().revents = 0;
-}
-
-void Server::handle_client(std::vector<pollfd>::iterator& it, int i)
-{
-	char buf[4096];
-	int sender_fd = fds[i].fd;
-	size_t nbytes = recv(sender_fd, buf, 4096, 0);
-
-	if (nbytes <= 0)
+	struct sockaddr_in client_addr;
+	socklen_t client_addr_len = sizeof(client_addr);
+	int client_fd;
+	pollfd client;
+	if ((client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len)) < 0)
 	{
-		if (nbytes < 0)
-			;// print error, not throw???
-		close(fds[i].fd);
-		fds.erase(it);
-		it = fds.begin();
+		perror("accept");
+		return;
+		//exit(1);
 	}
+	fcntl(client_fd, F_SETFL, O_NONBLOCK);
+	client.fd = client_fd;
+	client.events = POLLIN | POLLHUP | POLLERR;
+	client.revents = 0;
+	poll_set.push_back(client);
+}
+
+void server::get_data_from_client(int i)
+{
+		char buf[BUF_SIZE];
+		std::string data;
+		int ret = recv(poll_set[i].fd, buf, BUF_SIZE, 0);
+		if(ret < 0){ return;}
+		else if(ret == 0){clear_fd(i);}
+		else
+		{
+			data = buf;
+			std::string path = data.substr(data.find("/"), data.find("HTTP") - 4);
+			for (unsigned int i = 0; i < contents.size(); i++)
+		{
+           if (path.find(contents[i]) != std::string::npos)
+		   {
+			   std::string root_ext;
+			   if(contents[i] == ".css")
+				   root_ext = "/styles";
+			   else if(contents[i] == ".html")
+				    continue;
+			   else if(contents[i] == ".js")
+				   root_ext = "/scripts";
+			   else
+				   root_ext = "/images";
+			   std::cout << this->serveInfo.locations["/"].root + root_ext  << path << std::endl;
+
+			   std::cout << content_typer(contents,i) << std::endl;
+			   std::string pathed = trim(this->serveInfo.locations["/"].root + root_ext +  path);
+			   std::ifstream file;
+			   file.open(pathed.c_str());
+			   if (!file.is_open())
+			   {
+				   //  what to send to request when the content is not found
+				   std::cout << "file not found" << std::endl;
+			   }
+			   else
+			   {
+				   file.close();
+				   std::string content_type = content_typer(contents, i);
+				    resp = response(pathed, content_type);
+					return;
+			   }
+			}
+		}
+			resp =  response(find_page(*this, data),this->serveInfo.error_pages,data);
+			//poll_set[i].revents = 0 | POLLOUT | POLLHUP | POLLERR;
+		}
+}
+void server::get_data_from_server(int i)
+{
+	std::string http_response =  resp.build_response();
+	int ret = send(poll_set[i].fd, http_response.c_str(), http_response.length(), 0);
+	if(ret < 0){ return;}
+	else if(ret == 0){clear_fd(i);}
 	else
 	{
-		for (size_t j = 0; j < nbytes; j++)
-			request.push_back(buf[j]);
-
-		// Request parsing
-
-		std::string header = ""; // Get Request Header
-		std::string body = ""; // Get Request Body
-
-		size_t size = header.length() + body.length() + 1;
-
-		char buffer[size];
-		bzero(buffer, size);
-
-		memcpy(buffer, header.data(), header.length());
-		memcpy(buffer + header.length(), body.data(), body.length());
-
-		send(sender_fd, buffer, size, 0);
-		bzero(buf, 4096);
-		request.clear();
+		clear_fd(i);
 	}
+	std::cout << "closed" << std::endl;
+	(void)ret;
 }
-
-std::vector<server_info>&Server::getServers(void)
+void server::run()
 {
-	return (servers);
-}
-
-void Server::run(void)
-{
-	int ret;
-
-	while (true)
+	poll (poll_set.data(),poll_set.size() , 50);
+	for(unsigned long i = 0; i <  poll_set.size();i++)
 	{
-		if ((ret = poll(&(fds.front()), fds.size(), 10000)) <= 0)
+		if(poll_set[i].revents & POLLIN)
 		{
-			if (ret == 0)
-				throw std::runtime_error("Request Timeout [408]");
-			if (ret == -1)
-				throw std::runtime_error("Internal Server Error [500]");
-		}
-
-		std::vector<pollfd>::iterator it;
-		for (it = fds.begin(); it != fds.end(); it++)
-		{
-			if (it->revents & POLLIN) {
-				for (size_t i = 0; i < fds.size(); i++) {
-					if (it->fd == fds[i].fd)
-						handle_listen(i);
-					else
-						handle_client(it, i);
+			if(poll_set[i].fd == server_fd)
+				add_client();
+			else
+			{
+				get_data_from_client(i);
+				if(poll_set[i].revents & POLLIN)
+				{
+					get_data_from_server(i);
 				}
 			}
+		}
+		if (poll_set[i].revents & POLLOUT )
+		{
+			get_data_from_server(i);
 		}
 	}
-}
-
-void Server::printServers(void)
-{
-	std::vector<server_info>::iterator it = servers.begin();
-	for (; it != servers.end(); it++) {
-		std::cout << "\n-------------------------------\n\n";
-		std::cout << "host : " << it->host << "\n";
-		std::cout << "port : " << it->port << "\n";
-
-		std::cout << "server_names : " << it->server_names << "\n";
-
-		std::cout << "error_pages : ";
-		if (it->error_pages.size() == 0)
-			std::cout << "undefined\n";
-		else {
-			std::cout << "\n";
-			std::map<int, std::string>::iterator mit = it->error_pages.begin();
-			for (; mit != it->error_pages.end(); mit++) {
-				std::cout << "    " << mit->first << " " << mit->second << "\n";
-			}
-		}
-
-		std::cout << "client_max_body_size : " << it->client_max_body_size << "\n";
-		std::cout << "server_fd : " << it->server_fd << "\n";
-
-		std::cout << "locations : ";
-		if (it->locations.size() == 0)
-			std::cout << "undefined\n";
-		else {
-			std::cout << "\n";
-			std::map<std::string, location_info>::iterator mit2 = it->locations.begin();
-			for (; mit2 != it->locations.end(); mit2++) {
-				std::cout << mit2->first << "\n";
-				std::cout << "    root : " << mit2->second.root << "\n";
-				std::cout << "    index : " << mit2->second.index << "\n";
-				std::cout << "    upload_dir : " << mit2->second.upload_dir << "\n";
-				std::cout << "    autoindex : " << ((mit2->second.autoindex) ? "true" : "false") << "\n";
-
-				std::cout << "    cgi : ";
-				if (mit2->second.cgi.size() == 0)
-					std::cout << "undefined\n";
-				else {
-					std::cout << "\n";
-					std::map<std::string, std::string>::iterator mit3 = mit2->second.cgi.begin();
-					for (; mit3 != mit2->second.cgi.end(); mit3++) {
-						std::cout << "        " << mit3->first << " " << mit3->second << "\n";
-					}
-				}
-
-				std::cout << "    allowed_requests : ";
-				if (mit2->second.allowed_requests.size() == 0)
-					std::cout << "undefined\n";
-				else {
-					std::cout << "\n";
-					std::vector<std::string>::iterator it2 = mit2->second.allowed_requests.begin();
-					for (; it2 != mit2->second.allowed_requests.end(); it2++) {
-						std::cout << "        " << *it2 << "\n";
-					}
-				}
-			}
-		}
-	}
-
-	std::cout << "\n-------------------------------\n";
 }
